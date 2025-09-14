fd21eb94b62a30919d8f5a9873ee73ae16f875d2
Arpit Gupta <arpitguptafzd123@gmail.com>
2025-09-14 20:21:44 +0530

    Deploy Ready

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,45 +1,56 @@
 import express, { json, urlencoded } from "express";
 import session from "express-session";
-import dotenv from "dotenv";
 import passport from "passport";
 import cors from "cors";
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+
 import dbConnect from "./config/dbConnect.js";
 import authRoutes from "./routes/authRoutes.js";
 import "./config/passportConfig.js";
 
-dotenv.config();
+if (!process.env.SESSION_SECRET) {
+  console.error("FATAL ERROR: SESSION_SECRET is not defined.");
+  process.exit(1);
+}
+
 dbConnect();
 
 const app = express();
 
-// MiddleWares
-
 const corsOptions = {
-  origin: ["http://localhost:3001"],
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
+
+
 app.use(cors(corsOptions));
 app.use(json({ limit: "100mb" }));
 app.use(urlencoded({ limit: "100mb", extended: true }));
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || "secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 60000 * 60,
+      maxAge: 60000 * 60, //
     },
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Routes
+
 app.use("/api/auth", authRoutes);
 
-//Listen app
 
 const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
